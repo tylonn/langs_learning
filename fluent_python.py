7f38b3aa0b7f25550f872f10822cae42d99a4f9b
TheilonMacedo <theilonhenrique@gmail.com>
2024-02-19 00:50:11 -0300

    New notes

--- fluent_python.py
+++ fluent_python.py
@@ -1,1 +1,8 @@
-print("test")
+class BaseClass:
+    def __init_subclass__(cls, my_name):
+        print(f"Subclass created and my name is {my_name}")
+        # super().__init_subclass__() Needed only in case of
+        # multiple inheritance
+
+
+MyDynamicSubClass = type("MyDynamicSubClass", (BaseClass,), {}, my_name="John")
